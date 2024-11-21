64d3feb8a1be49a70e187e2ac0369ed59d783c29
Kyle <kylefink3@gmail.com>
2024-11-21 18:21:46 -0500

    enum stuff?

--- Cooking.c
+++ Cooking.c
@@ -2,9 +2,25 @@
 #include <semaphore.h>
 #include <pthread.h>
 #include <stdlib.h>
+	
+
+
+enum INGREDIENTS {
+	FLOUR,
+	SUGAR,
+	YEAST,
+	BAKING_SODA,
+	SALT,
+	CINNAMON,
+	EGGS,
+	MILK,
+	BUTTER
+};
 
 //----------------------RECIPIES-----------------------------
 struct Cookies{
+		
+
 	bool flour;
 	bool sugar;
 	bool milk;
@@ -29,7 +45,7 @@ struct SoftPretzel{
 	bool sugar;
 	bool salt;
 	bool yeast;
-	bool bakingSoda;
+	bool bakingSoda; 
 	bool egg;
 }
 struct CinnamonRolls{
@@ -52,9 +68,9 @@ struct pantryData{
 }
 
 struct fridgeData{
-	int Eggs 	= 1;
-	int Milk 	= 2;
-	int Butter 	= 3;
+	int Eggs 	= 7;
+	int Milk 	= 8;
+	int Butter 	= 9;
 }
 
 //-------------------SEMAPHORE STUFF-------------------------
@@ -70,12 +86,12 @@ sem_t semOven;
 //struct sembuf using = {0, -1, SEM_UNDO};
 //struct sembuf done = {0, +1, SEM_UNDO};
 
-sem_init(&semPantry, 0, 1) // 1 pantry
-sem_init(&semFridge, 0, 2) // 2 fridges
-sem_init(&semMixer, 0, 2) // 2 mixers
-sem_init(&semBowl, 0, 3) // 3 bowls
-sem_init(&semSpoon, 0, 5) // 5 spoons
-sem_init(&semOven, 0, 1) // 1 Oven
+//sem_init(&semPantry, 0, 1) // 1 pantry
+//sem_init(&semFridge, 0, 2) // 2 fridges
+//sem_init(&semMixer, 0, 2) // 2 mixers
+//sem_init(&semBowl, 0, 3) // 3 bowls
+//sem_init(&semSpoon, 0, 5) // 5 spoons
+//sem_init(&semOven, 0, 1) // 1 Oven
 
 //---------------------THREAD STUFF--------------------------
 //location where the new thread is stored (I think the ids of the bakers)
@@ -87,9 +103,36 @@ int threadArgument;
 
 // baker thread
 void* baker (void* arg){
+	// decide what recipe to do
+
+	// loop for doing recipe
+	// while(recipe not done)
 	
+		// look at recipe and choose get what ingredients to grab
+			// add all the ingredients needed to an array	
+	
+		// loop to grab ingredients 
+		// while(ingredientsArray[] != NULL)
+			// sem_wait(semaphore name) to wait to access
 		
+			// access and grab the one thing
+				// check for numbers that are in the array
+				// remove ingredient number from array 
+
+			// sem_post(semaphore name)
+			// set that thing to true to mark that it has it	
+		// loop
+
+		// wait for the bowl mixer and spoon to make 
+			// simulate time to mix ingredients
+		// wait for the oven 
+			// simulate time to cook ingredients
 	
+		// tell recipe done
+	
+	// loop back to next recipe
+
+
 }
 //-------------------------MAIN------------------------------
 	struct Cookies *cookieRecipe;
@@ -97,6 +140,13 @@ void* baker (void* arg){
 	struct HomeMadePizzaDough *pizzaRecipe;
 	struct SoftPretzel *pretzelRecipe;
 	struct CinnamonRolls *rollsRecipe;
+	
+	sem_init(&semPantry, 0, 1) // 1 pantry
+	sem_init(&semFridge, 0, 2) // 2 fridges
+	sem_init(&semMixer, 0, 2) // 2 mixers
+	sem_init(&semBowl, 0, 3) // 3 bowls
+	sem_init(&semSpoon, 0, 5) // 5 spoons
+	sem_init(&semOven, 0, 1) // 1 Oven
 
 	printf("Please enter the number of bakers.");
 	// again not sure if threadargument is the right variable 
@@ -104,26 +154,11 @@ void* baker (void* arg){
 
 	// create a thread for each baker
 	for(int i = 0; i < threadArgument; i++){
-		pthread_create(&bakerId, &baker_attr, baker, &threadArgument)
+		pthread_create(&bakerId, &baker_attr, baker, &threadArgument);
 	}
 	
 
 
-
-	//
-	// creating semids
-	// Check if this way works here TODO********************************
- 
-	// get the semId for pantry
-	if((semPantryId = semget(IPC_PRIVATE, 1, S_IRUSR|S_IWUSR)) == -1){
-		perror("Semget: semget failed");
-		exit(0);
-	}
-	// get the semId for fridge
-	if((semFridgeId = semget(IPC_PRIVATE, 1, S_IRUSR|S_IWUSR)) == -1){
-		perror("Semget: semget failed");
-		exit(0);
-	}
 	
 	return 0;
 }
